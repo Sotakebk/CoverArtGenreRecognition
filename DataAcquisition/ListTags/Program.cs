e6d3c6509db7dfe819c9efa804b279bdd670ec42
Sota <81926249+Sotakebk@users.noreply.github.com>
2022-05-18 19:05:18 +0200

    ListTags list tags which are referenced by releases or releaseGroups of unknown genre

--- DataAcquisition/ListTags/Program.cs
+++ DataAcquisition/ListTags/Program.cs
@@ -25,21 +25,62 @@ namespace DataAcquisition.ListTags
             Console.WriteLine("Loading release group to tag references...");
             var releaseGroupReferences = TagRelationReader.ReadReleaseGroupToTagRelations();
 
-            Console.WriteLine("Calculating reference counts...");
-            var referenceCounts = new Dictionary<int, int>(tags.Count);
-            IncrementCounters(referenceCounts, releaseReferences, tags);
-            IncrementCounters(referenceCounts, releaseGroupReferences, tags);
+            {
+                Console.WriteLine("Calculating reference counts for all releases and groups...");
+                var referenceCounts = new Dictionary<int, int>(tags.Count);
+                IncrementCounters(referenceCounts, releaseReferences, tags);
+                IncrementCounters(referenceCounts, releaseGroupReferences, tags);
 
-            Console.WriteLine("Sorting...");
-            var entries = referenceCounts.Select(i => (count: i.Value, tag: tags[i.Key]))
-                .OrderByDescending(i => i.count)
-                .ToArray();
+                Console.WriteLine("Sorting...");
+                var entries = referenceCounts.Select(i => (count: i.Value, tag: tags[i.Key]))
+                    .OrderByDescending(i => i.count)
+                    .ToArray();
 
-            Console.WriteLine("Saving...");
-            SaveTags(entries.Where(i => GenreHelper.IsStringInAnyGroup(i.tag)), "known_tags_list.txt");
-            SaveTags(entries.Where(i => !GenreHelper.IsStringInAnyGroup(i.tag)), "unknown_tags_list.txt");
+                Console.WriteLine("Saving...");
+                SaveTags(entries.Where(i => GenreHelper.IsStringInAnyGroup(i.tag)), "known_tags_all_references.txt");
+                SaveTags(entries.Where(i => !GenreHelper.IsStringInAnyGroup(i.tag)), "unknown_tags_all_references.txt");
+            }
+
+            {
+                Console.WriteLine("Calculating reference counts for releases and groups with no known genre...");
+                var releasesWithKnownGenre = GetSetOfObjectsWithKnownGenre(releaseReferences, tags);
+                var releaseGroupsWithKnownGenre = GetSetOfObjectsWithKnownGenre(releaseGroupReferences, tags);
+
+                var releaseWithNoGenreRefs = releaseReferences.Where(tuple => !releasesWithKnownGenre.Contains(tuple.releaseId)).ToArray();
+                var releaseGroupsWithNoGenreRefs = releaseGroupReferences.Where(tuple => !releaseGroupsWithKnownGenre.Contains(tuple.groupId)).ToArray();
+                
+                var referenceCounts = new Dictionary<int, int>(tags.Count);
+                IncrementCounters(referenceCounts, releaseWithNoGenreRefs, tags);
+                IncrementCounters(referenceCounts, releaseGroupsWithNoGenreRefs, tags);
+
+                Console.WriteLine("Sorting...");
+                var entries = referenceCounts.Select(i => (count: i.Value, tag: tags[i.Key]))
+                    .OrderByDescending(i => i.count)
+                    .ToArray();
+
+                Console.WriteLine("Saving...");
+                SaveTags(entries.Where(i => GenreHelper.IsStringInAnyGroup(i.tag)), "known_tags_unknown_genre_references.txt");
+                SaveTags(entries.Where(i => !GenreHelper.IsStringInAnyGroup(i.tag)), "unknown_tags_unknown_genre_references.txt");
+
+            }
+
 
             QuitImmediately("Work done!");
+        }
+
+        private static HashSet<int> GetSetOfObjectsWithKnownGenre((int, int)[] references, Dictionary<int, string> tags)
+        {
+            var set = new HashSet<int>();
+            foreach (var (objectId, tagId) in references)
+            {
+                var tag = tags[tagId];
+                if (GenreHelper.IsStringInAnyGroup(tag))
+                {
+                    set.Add(objectId);
+                }
+            }
+
+            return set;
         }
 
         private static void SaveTags(IEnumerable<(int, string)> entries, string file)
